54d1b2b51285b0539052791477da7f4005d3f434
mikronman <mkleczkajr@gmail.com>
2023-03-31 09:04:27 -0400

    quick change to deal with self-signed SSL issue. Need to bypass heroku's requirement for now.

--- src/db.js
+++ src/db.js
@@ -3,11 +3,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // For now we are going to disable Heroku's auth check when hitting this API from local
-const sslConfig = process.env.SSL_REJECT_UNAUTHORIZED === 'false' ? { rejectUnauthorized: false } : true;
+// const sslConfig = process.env.SSL_REJECT_UNAUTHORIZED === 'false' ? { rejectUnauthorized: false } : true;
+const sslAuth = process.env.SSL_REJECT_UNAUTHORIZED
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: sslConfig,
+    ssl: sslAuth,
+    connectionString: process.env.DATABASE_URL
 });
 
 client.connect();
